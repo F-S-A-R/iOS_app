4dc5dc15deff2b892a32f05b669e129de996ddac
Rich Downie <rich@blackboxd.com>
2021-01-02 10:33:53 -0500

    refacgtor

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,31 +2,3 @@ require 'appium_lib'
 require 'byebug'
 require 'rspec'
 require 'eyes_appium'
-
-caps = {}
-# Set URL of the application under test
-caps['app'] = (File.join(File.dirname(__FILE__), 'FSAR.app'))
-
-# Specify device and os_version for testing
-caps['deviceName'] = 'iPhone 11 Pro'
-caps['platformVersion'] = '14.3'
-caps['automationName'] = 'XCUITest'
-
-#Set the platform name
-caps['platformName'] = 'iOS'
-
-# Initialize the remote Webdriver and desired capabilities defined above
-appium_driver = Appium::Driver.new({
-                                       caps: caps,
-                                       appium_lib: {
-                                           server_url: "http://0.0.0.0:4723/wd/hub"
-                                       }}, true)
-                                       
-                                       
-Before do
-  @driver = appium_driver.start_driver
-end
-
-After do
-  @driver.quit
-end
---
+++ features/support/hooks.rb
@@ -0,0 +1,35 @@
+caps = {}
+# Set URL of the application under test
+caps['app'] = (File.join(File.dirname(__FILE__), 'FSAR.app'))
+
+# Specify device and os_version for testing
+caps['deviceName'] = 'iPhone 11 Pro'
+caps['platformVersion'] = '14.3'
+caps['automationName'] = 'XCUITest'
+
+#Set the platform name
+caps['platformName'] = 'iOS'
+
+# Initialize the remote Webdriver and desired capabilities defined above
+appium_driver = Appium::Driver.new({
+                                       caps: caps,
+                                       appium_lib: {
+                                           server_url: "http://0.0.0.0:4723/wd/hub"
+                                       }}, true)
+
+
+AfterConfiguration do
+  $eyes = Applitools::Appium::Eyes.new
+  $eyes.api_key = 'mpPWTHC2CNz5wPajbzzo8U104mOQ0s2HcfbOEILr1iC108g110'
+  p $eyes.match_level
+end
+
+Before do
+  @driver = appium_driver.start_driver
+end
+
+After do
+  @driver.quit
+  $eyes.close
+  # eyes.abort_if_not_closed
+end
