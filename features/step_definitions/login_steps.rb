6086b14fe69e082d3aa01d8841eb49fd989482f0
Rich Downie <rich@blackboxd.com>
2021-01-02 08:00:52 -0500

    updates

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,7 +1,12 @@
 Given /I login as "(.*)"/ do |email|
-  @driver.find_element(:accessibility_id, 'EmailTxtField').clear
-  @driver.find_element(:accessibility_id, 'EmailTxtField').send_keys email
-  @driver.find_element(:accessibility_id, 'PasswordTxtField').clear
-  @driver.find_element(:accessibility_id, 'PasswordTxtField').send_keys "supersecret"
-  @driver.find_element(:accessibility_id, 'Sign In').click
+  eyes = Applitools::Selenium::Eyes.new
+  eyes.api_key = 'mpPWTHC2CNz5wPajbzzo8U104mOQ0s2HcfbOEILr1iC108g110'
+  eyes.test(app_name: 'FSAR', test_name: 'Login Page test', driver: @driver) do
+    @driver.find_element(:accessibility_id, 'EmailTxtField').clear
+    @driver.find_element(:accessibility_id, 'EmailTxtField').send_keys email
+    @driver.find_element(:accessibility_id, 'PasswordTxtField').clear
+    @driver.find_element(:accessibility_id, 'PasswordTxtField').send_keys "supersecret"
+    byebug
+    @driver.find_element(:accessibility_id, 'Sign In').click
+  end
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,9 @@
 require 'appium_lib'
 require 'selenium-webdriver'
-require 'pry'
+require 'byebug'
 require 'rspec'
+require 'eyes_appium'
+require 'eyes_selenium'
 
 caps = {}
 # Set URL of the application under test
@@ -15,12 +17,19 @@ caps['automationName'] = 'XCUITest'
 #Set the platform name
 caps['platformName'] = 'iOS'
 
+
+# eyes = Applitools::Eyes.new
+# # This is your api key, make sure you use it in all your tests.
+# eyes.api_key = 'mpPWTHC2CNz5wPajbzzo8U104mOQ0s2HcfbOEILr1iC108g110'
+
 # Initialize the remote Webdriver and desired capabilities defined above
 appium_driver = Appium::Driver.new({
-                                       'caps' => caps,
-                                       'appium_lib' => {
-                                           :server_url => "http://0.0.0.0:4723/wd/hub"
+                                       caps: caps,
+                                       appium_lib: {
+                                           server_url: "http://0.0.0.0:4723/wd/hub"
                                        }}, true)
+                                       
+                                       
 Before do
   @driver = appium_driver.start_driver
 end
