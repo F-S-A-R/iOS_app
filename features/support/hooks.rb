3e2a1a29d4ff3ed9ccad0eb5f71851e7775a0dcf
Rich Downie <rich@blackboxd.com>
2021-01-02 20:09:25 -0500

    updates

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,21 +1,3 @@
-caps = {}
-# Set URL of the application under test
-caps['app'] = (File.join(File.dirname(__FILE__), 'FSAR.app'))
-
-# Specify device and os_version for testing
-caps['deviceName'] = 'iPhone 11 Pro'
-caps['platformVersion'] = '14.3'
-caps['automationName'] = 'XCUITest'
-caps['platformName'] = 'iOS'
-
-# Initialize the remote Webdriver and desired capabilities defined above
-appium_driver = Appium::Driver.new({
-                                       caps: caps,
-                                       appium_lib: {
-                                           server_url: "http://0.0.0.0:4723/wd/hub"
-                                       }}, true)
-
-
 AfterConfiguration do
   $eyes = Applitools::Appium::Eyes.new
   $eyes.api_key = 'mpPWTHC2CNz5wPajbzzo8U104mOQ0s2HcfbOEILr1iC108g110'
@@ -23,7 +5,7 @@ AfterConfiguration do
 end
 
 Before do |scenario|
-  @driver = appium_driver.start_driver
+  @driver = $appium_driver.start_driver
   @driver.manage.timeouts.implicit_wait = 5
   $eyes.open(app_name: 'FSAR', test_name: "#{scenario.name}", driver: @driver)
 end
