6e6e23c512b949cb55efb6b39ff58180475dca8e
Rich Downie <rich@blackboxd.com>
2023-11-27 12:55:15 -0500

    converting to RSPEC

--- features/login.feature
+++
@@ -1,5 +0,0 @@
-@login
-Feature: Login Page
-
-  Scenario: Verify Login page
-    Given I login as "rich@fsar.com"
--- features/step_definitions/login_steps.rb
+++
@@ -1,8 +0,0 @@
-Given 'I login as {string}' do |email|
-  @driver.find_element(:accessibility_id, 'EmailTxtField').clear
-  @driver.find_element(:accessibility_id, 'EmailTxtField').send_keys email
-  @driver.find_element(:accessibility_id, 'PasswordTxtField').clear
-  @driver.find_element(:accessibility_id, 'PasswordTxtField').send_keys 'supersecret'
-  @driver.find_element(:accessibility_id, 'Sign In').click
-  $eyes.check_window('Login Page')
-end
